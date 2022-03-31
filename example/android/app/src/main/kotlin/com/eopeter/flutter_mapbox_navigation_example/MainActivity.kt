13d2cf301f28f560ffae35aec1f0e06aaf654ef6
Joseph Crowell <joseph.w.crowell@gmail.com>
2022-03-31 15:23:02 +1000

    remove unnecessary brackets from around MainActivity

--- example/android/app/src/main/kotlin/com/eopeter/flutter_mapbox_navigation_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/eopeter/flutter_mapbox_navigation_example/MainActivity.kt
@@ -2,6 +2,4 @@ package com.eopeter.flutter_mapbox_navigation_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-    
-}
+class MainActivity: FlutterActivity()
